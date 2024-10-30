00201fcfb472a8b13c94e66e6d18fb97086390f7
Weerawut Chaiyasomboon <weerawut@live.com>
2024-10-30 14:47:40 +0700

    Initial Commit

---
+++ BBQuotes17/BBQuotes17App.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotes17App.swift
+//  BBQuotes17
+//
+//  Created by Weerawut Chaiyasomboon on 30/10/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotes17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBQuotes17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BBQuotes17
+//
+//  Created by Weerawut Chaiyasomboon on 30/10/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
